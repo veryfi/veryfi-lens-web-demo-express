b0a8c38364d9f5bf6c226902810d179e54f4c301
Alejandro Cano <alejocano22@gmail.com>
2023-01-31 17:04:41 -0500

    WEB-2679: Fix camera bug fix on Lens for web demo

--- index.js
+++ index.js
@@ -1,51 +1,49 @@
-const demoCampaignCtx = document.getElementById("demo-campaign-ctx");
-const veryfiLensCtx = document.getElementById("veryfi-lens-ctx");
-const croppedImage = document.getElementById("veryfi-lens-cropped-image");
-const preview = document.getElementById("veryfi-preview");
+GET_SESSION_URL = 'http://localhost:5001/session';
 
-veryfiLensCtx.style.display = "none";
-
-GET_SESSION_URL = "http://localhost:5001/session";
+const demoCampaignCtx = document.getElementById('demo-campaign-ctx');
+const veryfiLensCtx = document.getElementById('veryfi-lens-ctx');
+const croppedImage = document.getElementById('veryfi-lens-cropped-image');
+const preview = document.getElementById('veryfi-preview');
+veryfiLensCtx.style.display = 'none';
 
 const init = async () => {
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-        },
-    };
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    },
+  };
 
-    const session = await fetch(GET_SESSION_URL, requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-            return data.session;
-        })
-        .catch((error) => {
-            return error;
-        });
+  const session = await fetch(GET_SESSION_URL, requestOptions)
+    .then((response) => response.json())
+    .then((data) => data.session)
+    .catch((error) => error);
     VeryfiLens.setLensSessionKey(session);
     VeryfiLens.init();
 };
 
-init();
-
 const capture = () => {
-    const image = VeryfiLens.capture();
-    croppedImage.src = `data:image/png;base64,${image}`;
-    preview.style.display = "block";
-    veryfiLensCtx.style.display = "none";
-    demoCampaignCtx.style.display = "block";
+  const image = VeryfiLens.capture();
+  croppedImage.src = `data:image/png;base64,${image}`;
+  preview.style.display = 'block';
+  veryfiLensCtx.style.display = 'none';
+  demoCampaignCtx.style.display = 'block';
+  document.body.style = 'overflow: auto';
 };
 
 const startCamera = () => {
-    VeryfiLens.startCamera();
-    veryfiLensCtx.style.display = "block";
-    demoCampaignCtx.style.display = "none";
+  VeryfiLens.startCamera();
+  veryfiLensCtx.style.display = 'block';
+  demoCampaignCtx.style.display = 'none';
+  document.body.style = 'overflow: hidden';
 };
 
 const stopCamera = () => {
-    VeryfiLens.stopCamera();
-    veryfiLensCtx.style.display = "none";
-    demoCampaignCtx.style.display = "block";
+  VeryfiLens.stopCamera();
+  veryfiLensCtx.style.display = 'none';
+  demoCampaignCtx.style.display = 'block';
 };
+
+// Get session //
+init();
--- server/index.js
+++ server/index.js
@@ -1,44 +1,38 @@
-const express = require("express");
-const axios = require("axios");
-var cors = require("cors");
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 
 const PORT = 5001;
-const VALIDATE_URL = "https://lens-pwa-api.veryfi.com/rest/validate_partner";
-const CLIENT_ID = "YOUR_CLIENT_ID_HERE"; // make sure to keep it as a secret
+const VALIDATE_URL = 'https://lens-pwa-api.veryfi.com/rest/validate_partner';
+const CLIENT_ID = 'YOUR_CLIENT_ID_HERE'; // make sure to keep it as a secret
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/session", getSession);
+app.post('/session', getSession);
 
 app.listen(PORT, function (err) {
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
+  if (err) console.log(err);
+  console.log('Server listening on PORT', PORT);
 });
 
 async function getSession(request, response) {
-    const session = await getVeryfiSession(CLIENT_ID);
-    response.send(session);
+  const session = await getVeryfiSession(CLIENT_ID);
+  response.send(session);
 }
 
 async function getVeryfiSession(clientId) {
-    return await axios
-        .post(
-            VALIDATE_URL,
-            {},
-            {
-                headers: {
-                    "CLIENT-ID": clientId,
-                },
-            }
-        )
-        .then((response) => {
-            return {
-                session: response.data.session,
-            };
-        })
-        .catch((error) => {
-            return error;
-        });
+  return await axios.post(
+    VALIDATE_URL,
+    {},
+    {
+      headers: {
+        'CLIENT-ID': clientId,
+      },
+    }).then((response) => {
+      return {
+        session: response.data.session
+      }
+    }).catch((error) => error);
 }
